0e07f361817fec882523d3d890d6104640eea7eb
GuoJikun <guojikun486546@163.com>
2023-07-19 12:02:51 +0800

    ✨ feat: 关闭部分eslint规则

--- packages/ivy-design-wc/.eslintrc.cjs
+++ packages/ivy-design-wc/.eslintrc.cjs
@@ -2,14 +2,18 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    root: true,
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        'vue/multi-word-component-names': 'off',
+        'vue/no-reserved-component-names': 'off'
+    }
 }
