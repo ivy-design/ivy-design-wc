0b5f27302139bb4aa5359238d63cef17bb1151cd
GuoJikun <guojikun486546@163.com>
2023-07-21 17:17:24 +0800

    wip: 开发 Tabs 组件

---
+++ packages/ivy-design-wc/src/components/tabs/pane.ce.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineOptions({
+  name: "TabPane",
+})
+
+defineProps({
+  label: String,
+  index: String,
+  disabled: Boolean,
+  show: Boolean,
+})
+
+</script>
+
+<template>
+  <slot></slot>
+</template>
+
+<style lang="scss">
+:host {
+  display: none;
+}
+:host([show]) {
+  display: block;
+}
+</style>
