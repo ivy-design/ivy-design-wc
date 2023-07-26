9dc9e72d5f34590609ce3aa60991a1bbc3802b4d
GuoJikun <guojikun486546@163.com>
2023-07-26 17:53:50 +0800

    feat: 增加 Drawer 组件

---
+++ packages/ivy-design-wc/src/use/useHostElement.ts
@@ -0,0 +1,12 @@
+import { ref } from 'vue'
+/***
+ * 获取当前组件的宿主元素-通过 ref 的上级元素获取
+ ***/
+export const useHostElement = () => {
+    const el = ref<HTMLElement | null>(null)
+    const getHostElement = () => (el.value.parentNode as ShadowRoot).host
+    return {
+        el,
+        getHostElement
+    }
+}
