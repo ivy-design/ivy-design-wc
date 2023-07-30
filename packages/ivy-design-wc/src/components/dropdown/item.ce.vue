fae929790551e83c1e52171696538044490e6fa7
GuoJikun <guojikun486546@163.com>
2023-07-30 12:43:34 +0800

    feat: 增加 Dropdown 组件

---
+++ packages/ivy-design-wc/src/components/dropdown/item.ce.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+defineOptions({
+    name: 'DropdownItem'
+})
+
+defineProps({
+    divided: Boolean,
+    disabled: Boolean,
+    command: {
+        type: [String, null],
+        default: null
+    }
+})
+</script>
+
+<template>
+    <slot></slot>
+</template>
+
+<style lang="scss">
+:host {
+    display: block;
+    padding: 0 16px;
+    line-height: 32px;
+    height: 32px;
+    cursor: pointer;
+    transition: background-color 0.15s ease;
+    font-size: var(--ivy-dropdown-font-size);
+    user-select: none;
+    word-break: keep-all;
+}
+
+:host([disabled]) {
+    cursor: not-allowed;
+    color: #a8abb2;
+    background-color: transparent;
+    pointer-events: none;
+}
+:host(:hover) {
+    background-color: var(--ivy-border-color, #dcdfe6);
+}
+:host([divided]) {
+    height: 1px;
+    overflow: hidden;
+    color: transparent;
+    background-color: var(--ivy-border-color, #dcdfe6);
+    padding: 0;
+    pointer-events: none;
+}
+</style>
---
+++ packages/ivy-design-wc/src/components/dropdown/menu.ce.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineOptions({
+    name: 'DropdownMenu'
+})
+</script>
+
+<template>
+    <slot></slot>
+</template>
+
+<style lang="scss">
+:host {
+    display: block;
+    padding: 2px 0;
+}
+</style>
