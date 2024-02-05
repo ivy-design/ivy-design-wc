98e457d2d49a895cad11443092231b8a0d5a9a51
GuoJikun <guojikun486546@163.com>
2024-02-05 14:37:41 +0800

    build: 开发环境不生产types文件

--- packages/ivy-design-wc/vitest.config.ts
+++ packages/ivy-design-wc/vitest.config.ts
@@ -3,16 +3,19 @@ import { mergeConfig } from 'vite'
 import { configDefaults, defineConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-    viteConfig,
-    defineConfig({
-        test: {
-            environment: 'jsdom',
-            exclude: [...configDefaults.exclude, 'e2e/*'],
-            root: fileURLToPath(new URL('./', import.meta.url)),
-            testTransformMode: {
-                web: ['\\.[jt]sx$']
+export default defineConfig((configEnv) => {
+    const config = viteConfig(configEnv)
+    return mergeConfig(
+        config,
+        defineConfig({
+            test: {
+                environment: 'jsdom',
+                exclude: [...configDefaults.exclude, 'e2e/*'],
+                root: fileURLToPath(new URL('./', import.meta.url)),
+                testTransformMode: {
+                    web: ['\\.[jt]sx$']
+                }
             }
-        }
-    }) as never
-)
+        }) as never
+    )
+})
