8d2b636a96ebf408b2243ab8c2edaab4dde565ba
GuoJikun <guojikun486546@163.com>
2024-02-22 11:21:32 +0800

    docs: 增加 logo

--- packages/docs/src/.vitepress/config.js
+++ packages/docs/src/.vitepress/config.js
@@ -4,9 +4,11 @@ import sidebar from "./config/sidebar";
 import nav from "./config/nav";
 
 export default defineConfig({
-  title: "ivy-design",
+  title: "ivyDesign",
   description: "使用 vue3 开发的web component组件库",
+  head: [["link", { rel: "icon", href: "/favicon.png" }]],
   themeConfig: {
+    logo: "/images/logo.png",
     nav,
     sidebar,
     outline: [2, 3],
